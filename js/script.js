59618ea6aa8db9ddc8e940e461acaf395e7b1433
MoDev0 <mh38279@gmail.com>
2022-03-05 23:02:03 +0200

    Add eraser button

--- js/script.js
+++ js/script.js
@@ -1,49 +1,50 @@
 const container=document.querySelector('.container');
-// function createGrid(){
-//     for (let i=0;i<(16*16);i++){
-//         const div=document.createElement('div');
-//         div.className="item";
-    
-//         container.appendChild(div);
-
-//     }
-//     console.log(container);
-
-//     container.style.gridTemplateColumns="repeat(16,1fr)";
-
-// }
-
-for (let i=0;i<(16*16);i++){
-    const div=document.createElement('div');
-    div.className="item";
-
-    container.appendChild(div);
-
-}
-console.log(container);
-
-container.style.gridTemplateColumns="repeat(16,1fr)";
-
-
-
-
 
 const inputColor=document.getElementById("favcolor");
 let color="blue";
-
-
-const items=document.querySelectorAll('div.item');
-items.forEach(item => {
-item.addEventListener('mouseover',function(){item.style.backgroundColor=color;
-
-
-});
-    
-});
-
 //color : stores user's entered color
+
 inputColor.addEventListener('input',function(){
     color=inputColor.value;    
     
     });
+
+function createGrid(){
+for (let i=0;i<(16*16);i++){
+    const div=document.createElement('div');
+    div.className="item";
+    div.addEventListener('mouseover',function(){div.style.backgroundColor=color;})
+
+
+    container.appendChild(div);
+
+}
+
+container.style.gridTemplateColumns="repeat(16,1fr)";
+}
+
+
+let eraseBtn=document.getElementById("btn");
+eraseBtn.addEventListener('click',function(){
+
+   container.innerHTML='';
+   createGrid();
+
+
+ 
+});
+
+
+
+
+
+
+
+window.onload=()=>{
+    createGrid();
+
+
+}
+    
+       
     
