81019206226d76ca2afa715b7ee191874e5fd3bf
MoDev0 <mh38279@gmail.com>
2022-03-05 17:52:11 +0200

    Add eventlistener for all grid cells on mouseover

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
 const container=document.querySelector('.container');
-function createGrid(){
-    for (let i=0;i<(16*16);i++){
-        const div=document.createElement('div');
-        div.className="item";
+// function createGrid(){
+//     for (let i=0;i<(16*16);i++){
+//         const div=document.createElement('div');
+//         div.className="item";
     
-        container.appendChild(div);
+//         container.appendChild(div);
 
-    }
-    console.log(container);
+//     }
+//     console.log(container);
 
+//     container.style.gridTemplateColumns="repeat(16,1fr)";
+
+// }
+
+for (let i=0;i<(16*16);i++){
+    const div=document.createElement('div');
+    div.className="item";
+
+    container.appendChild(div);
 
 }
+console.log(container);
+
 container.style.gridTemplateColumns="repeat(16,1fr)";
+
+
+
+
+
+
+const items=document.querySelectorAll('div.item');
+items.forEach(item => {
+item.addEventListener('mouseover',function(){item.style.backgroundColor="black";
+
+
+});
+    
+});
